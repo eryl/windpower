bf5fe4c38901fed020366e08268dbeb18b1a37ce
Erik Ylipää <erik.ylipaa@gmail.com>
2020-06-02 22:06:40 +0200

    Minor edits to help debug

--- bin/merge_netcdf.py
+++ bin/merge_netcdf.py
@@ -15,17 +15,21 @@ def main():
     weather_coordinate_files = defaultdict(list)
     pattern = re.compile(r'(DWD_ICON-EU|NCEP_GFS)_(\d*\.\d*),(\d*\.\d*)_.*\.nc')
     for d in args.directories:
-        for f in d.glob('*.nc'):
+        netcdf_files = list(d.glob('*.nc'))
+        for f in netcdf_files:
             m = re.match(pattern, f.name)
             if m is not None:
                 model, lat, lon = m.groups()
                 weather_coordinate_files[(model, float(lat), float(lon))].append(f)
     for (model, lat, lon), files in tqdm(weather_coordinate_files.items()):
-        ds = xr.open_mfdataset(sorted(files))
-        unique_reference_times, indices = np.unique(ds['reference_time'].values, return_index=True)
-        filtered_ds = ds.isel(reference_time=indices)
-        args.output_dir.mkdir(exist_ok=True, parents=True)
-        filtered_ds.to_netcdf(args.output_dir / f"{model}_{lat},{lon}.nc")
+        try:
+            ds = xr.open_mfdataset(sorted(files), combine='by_coords')
+            unique_reference_times, indices = np.unique(ds['reference_time'].values, return_index=True)
+            filtered_ds = ds.isel(reference_time=indices)
+            args.output_dir.mkdir(exist_ok=True, parents=True)
+            filtered_ds.to_netcdf(args.output_dir / f"{model}_{lat},{lon}.nc")
+        except ValueError:
+            print(f"problematic files: {[f.name for f in files]}")
 
 
 if __name__ == '__main__':
