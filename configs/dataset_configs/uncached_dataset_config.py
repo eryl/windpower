7241ea1012fa0441b23d58e66ef8216f2d9095b5
Erik Ylipää <erik.ylipaa@gmail.com>
2020-06-09 12:04:38 +0200

    Removed old dataset configs

--- configs/dataset_configs/uncached_dataset_config.py
+++
@@ -1,9 +0,0 @@
-from windpower.dataset import DatasetConfig
-
-dataset_config = DatasetConfig(
-    window_length = 7,
-    production_offset = 3,
-    horizon = 30,
-    include_variable_index = False,
-)
-
