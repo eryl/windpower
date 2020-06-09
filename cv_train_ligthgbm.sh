e9acb27cb1744255f8ae2c8b155aef83f51e64c3
Erik Ylipää <erik.ylipaa@gmail.com>
2020-06-09 12:03:49 +0200

    Changed all models to use the correct dataset config

--- cv_train_ligthgbm.sh
+++ cv_train_ligthgbm.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source config_hp_train.sh
 MODEL_CONFIG=$CONFIG_DIR/model_configs/lightgbm.py
-DATASET_CONFIG=$CONFIG_DIR/dataset_configs/uncached_dataset_config.py
+DATASET_CONFIG=$CONFIG_DIR/dataset_configs/dataset_config.py
 TRAINING_CONFIG=$CONFIG_DIR/training_configs/single_epoch_single_cv_loop.py
 VARIABLE_CONFIG=$CONFIG_DIR/variable_configs/non_one_hot_experiment_variables.py
 CMD="python bin/train.py $VARIABLE_CONFIG $MODEL_CONFIG $DATASET_CONFIG $TRAINING_CONFIG $EXPERIMENT_DIR  $@"
--- hp_train_random_forest.sh
+++ hp_train_random_forest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 source config_hp_train.sh
-DATASET_CONFIG=$CONFIG_DIR/dataset_configs/uncached_dataset_config.py
+DATASET_CONFIG=$CONFIG_DIR/dataset_configs/dataset_config.py
 MODEL_CONFIG=$CONFIG_DIR/model_configs/hp_random_forest.py
 VARIABLE_CONFIG=$CONFIG_DIR/variable_configs/non_one_hot_experiment_variables.py
 CMD="python bin/train.py $VARIABLE_CONFIG $MODEL_CONFIG $DATASET_CONFIG $TRAINING_CONFIG $EXPERIMENT_DIR  $@"
