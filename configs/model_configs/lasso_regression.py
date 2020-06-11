c6a19550aa62defdcb6f82f546d3338a88b74148
Erik Ylipää <erik.ylipaa@gmail.com>
2020-06-11 18:08:39 +0200

    Updated lasso config

--- configs/model_configs/lasso_regression.py
+++ configs/model_configs/lasso_regression.py
@@ -1,8 +1,10 @@
 from sklearn.linear_model import Lasso
-from windpower.models import SklearnWrapper
+from windpower.models import SklearnWrapper, ModelConfig
 
 alpha = 0.0032
 
 model = SklearnWrapper
 base_args = tuple()
 base_kwargs = dict(model=Lasso, scaling=True, alpha=alpha, max_iter=5000)
+
+config = ModelConfig(model=model, model_args=base_args, model_kwargs=base_kwargs)
